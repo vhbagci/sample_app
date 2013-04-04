fa703104fed89600bdd54f43aaf46b13ca39441f
unknown <volkan_halil@SERHATG.sekerbilisim.com.tr>
2013-04-04 13:35:50 +0300

    Make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = user.email.downcase }
 
   validates :name,  :presence => true, length: { maximum: 50 } 
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, format: { with: VALID_EMAIL_REGEX },
   					uniqueness: { case_sensitive: false }
-  # Şekerbilişim REGEX: /\A\w+\.\w+@(cs.com.tr|sekerbilisim.com.tr)\z/i
+  # Şekerbilişim REGEX: /\A\w+\.\w+@(cs.com.tr|sekerbilisim.com.tr)\z/i 
+  validates :password, presence: true, length: { minimum: 6, maximum: 12}
+  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 12}
 end
